820b4dc932c2dfb9d9acd98a6cadabe59d65b485
Michael Herold <quabla@hemio.de>
2015-01-19 03:19:19 +0100

    Adds service_index

--- modules/user/functions.d/session_id.sql
+++ modules/user/functions.d/session_id.sql
@@ -3,7 +3,13 @@ description: |
  Gives an id for the database connection that is unique over all database connections.
  It is used to identify user logins.
 
+ Not sure if this stays unique with distributed infrastructure!
+
 return: varchar
 
 body: |
- RETURN session_user || '.' || pg_backend_pid();
+ RETURN 
+  session_user || '.' || 
+  pg_backend_pid() || '.' ||
+  (SELECT backend_start FROM pg_stat_get_activity(pg_backend_pid())) || '.' ||
+  pg_conf_load_time();
