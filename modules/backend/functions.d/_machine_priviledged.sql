5e1877f12ad582d688011b71ca87ecd1c935a953
Michael Herold <quabla@hemio.de>
2015-02-10 14:51:12 +0100

    Check backend priv

---
+++ modules/backend/functions.d/_machine_priviledged.sql
@@ -0,0 +1,36 @@
+name: _machine_priviledged
+description: |
+ Checks if a currently connected machine is priviledged to obtain data for
+ a certain service for a certain domain name.
+
+ WARNING: The parameter p_domain must be a domain, which means an entry in
+ the column dns.service.domain. It must not be confused with a service_name.
+
+returns: boolean
+
+templates:
+ - backend.backend
+
+parameters:
+ -
+  name: p_service
+  type: system.t_service
+ -
+  name: p_domain
+  type: dns.t_domain
+
+body: |
+    RETURN COALESCE(
+        (
+        SELECT TRUE FROM system.service_machine AS t
+            JOIN dns.service AS s
+            ON
+                s.service = p_service AND
+                s.domain = p_domain
+
+            WHERE
+                t.service = p_service AND
+                t.service_name = s.service_name AND
+                t.machine_name = v_machine
+        )
+    , FALSE);
