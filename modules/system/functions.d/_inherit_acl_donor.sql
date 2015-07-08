b0db23c1112a205e45513327c3d9c78f77b977b6
Michael Herold <quabla@hemio.de>
2015-07-08 23:42:38 +0200

    Silently stop if cycles are detected in ACL inheritance

--- modules/system/functions.d/_inherit_acl_donor.sql
+++ modules/system/functions.d/_inherit_acl_donor.sql
@@ -1,6 +1,6 @@
 name: _inherit_acl_donor
 description: |
- Returns all acl donors for a given user.
+ Returns all acl donors for a given user with their priority.
 
 returns: TABLE
 returns_columns:
@@ -20,17 +20,23 @@ parameters:
 
 body: |
  RETURN QUERY
- WITH RECURSIVE acl_donor(donor, priority_list) AS
+ WITH RECURSIVE acl_donor(donor, priority_list, cycle_detector) AS
  (
-    SELECT p_owner, CAST(ARRAY[] AS integer[])
+    -- cast to varchar, since arrays of t_user are not defined
+    SELECT p_owner, ARRAY[]::integer[], ARRAY[CAST(p_owner AS varchar)]
 
     UNION
 
-    SELECT curr.donor, array_append(prev.priority_list, curr.priority)
+    SELECT
+     curr.donor,
+     prev.priority_list || curr.priority),
+     cycle_detector || CAST(curr.donor AS varchar)
     FROM system.inherit_acl AS curr
-        JOIN acl_donor AS prev
-        ON prev.donor = curr.owner
+     JOIN acl_donor AS prev
+     ON
+      prev.donor = curr.owner AND
+      curr.donor <> ALL (prev.cycle_detector)
  )
  SELECT acl_donor.donor, acl_donor.priority_list FROM acl_donor
- -- Appending the NULL corrects the ordering between arrays with different size
- ORDER BY array_append(acl_donor.priority_list, NULL) DESC;
+ -- Appending the NULL changes the ordering between arrays with different size
+ ORDER BY acl_donor.priority_list || NULL DESC;
