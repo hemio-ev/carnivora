3d4b055a9b8c3aa4c9b64c5c8e0bad41dacbc5a4
Michael Herold <m.herold@fu-berlin.de>
2015-12-01 20:41:31 +0100

    [dns] Fixes problems in TXT rdata handling

---
+++ modules/commons/functions.d/_jsonb_to_array.sql
@@ -0,0 +1,12 @@
+name: _jsonb_to_array
+description: |
+ Converts a JSONB array to a PostgreSQL text[] array
+
+parameters:
+ - name: p_jsonb
+   type: jsonb
+
+returns: text[]
+
+body: |
+ RETURN ARRAY(SELECT jsonb_array_elements_text(p_jsonb));
